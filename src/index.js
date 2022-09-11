92e5977cdf9d90c35faa87904e3a6b551bca8c62
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2022-09-12 00:33:51 +0500

    Webpack config update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import { someFn } from 'test';
+import { someFn } from './test';
 
 someFn();
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
+    mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
-        filename: 'bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'build'),
+        clean: true,
     },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        }),
+    ],
 };
