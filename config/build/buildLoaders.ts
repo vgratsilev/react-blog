b96e01db3565ec21f83a6e2c91a2c543f0699005
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2022-09-13 01:13:55 +0500

    Added react & css loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,18 @@
 import webpack from 'webpack';
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+    const cssLoader = {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+        ],
+    };
+
     const typeScriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -9,5 +21,6 @@ export function buildLoaders(): webpack.RuleSetRule[] {
 
     return [
         typeScriptLoader,
+        cssLoader,
     ];
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -4,7 +4,7 @@ import { BuildEnv, BuildPath } from './config/build/types/config';
 import path from 'path';
 
 const paths: BuildPath = {
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
     build: path.resolve(__dirname, 'build'),
     html: path.resolve(__dirname, 'public', 'index.html'),
 };
