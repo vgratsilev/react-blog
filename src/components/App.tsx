be1570ff0b622dff6ea2bc491501d73e8d3b0699
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2022-09-15 01:46:28 +0500

    Added light/dark theme switcher

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,16 @@
-import '../index.scss';
+import '../styles/index.scss';
 import { Link, Route, Routes } from 'react-router-dom';
-// import AboutPage from '../pages/AboutPage/AboutPage';
-// import MainPage from '../pages/MainPage/MainPage';
 import { AboutPageAsync } from '../pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from '../pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
+import { useTheme } from '../theme/useTheme';
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className={'app'}>
+        <div className={`app ${theme}`}>
+            <button type={'button'} onClick={toggleTheme}>Change theme</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import { render } from 'react-dom';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
+import ThemeProvider from './theme/ThemeProvider';
 
 render(
     <BrowserRouter>
-        <App />
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
     </BrowserRouter>,
     document.getElementById('root'));
