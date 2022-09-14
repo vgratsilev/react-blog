370a2ed01a6e8c950b6ba1ec60e74a0ea72505bb
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2022-09-14 23:41:03 +0500

    Added React routing and code splitting
    Code splitting with lazy and suspense

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,22 @@
-import { Counter } from './Counter';
 import '../index.scss';
+import { Link, Route, Routes } from 'react-router-dom';
+// import AboutPage from '../pages/AboutPage/AboutPage';
+// import MainPage from '../pages/MainPage/MainPage';
+import { AboutPageAsync } from '../pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from '../pages/MainPage/MainPage.async';
+import { Suspense } from 'react';
 
 const App = () => {
     return (
         <div className={'app'}>
-            App text
-            <Counter />
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import { render } from 'react-dom';
 import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
 
 render(
-    <App />,
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
     document.getElementById('root'));
