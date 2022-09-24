a092de84bacfb1c887d2bd7529a86e426b364026
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2022-09-25 03:14:27 +0500

    Added Jest, added first test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,40 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+
+describe('classNames', () => {
+    test('with only first parameter', () => {
+        expect(classNames('mainClass')).toBe('mainClass');
+    });
+
+    test('with first parameter and array(third parameter)', () => {
+        const expected = 'mainClass second third';
+        expect(classNames('mainClass', {}, ['second', 'third'])).toBe(expected);
+    });
+
+    test('with mods', () => {
+        const expected = 'class1 class4 class2';
+        expect(
+            classNames(
+                'class1',
+                {
+                    class2: true,
+                    class3: false,
+                },
+                ['class4'],
+            ),
+        ).toBe(expected);
+    });
+
+    test('with mods undefined', () => {
+        const expected = 'class1 class4 class2';
+        expect(
+            classNames(
+                'class1',
+                {
+                    class2: true,
+                    class3: undefined,
+                },
+                ['class4'],
+            ),
+        ).toBe(expected);
+    });
+});
