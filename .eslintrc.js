fa17a4eaf963c281ebaf8aec056c2508ede64998
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2022-10-09 23:07:41 +0300

    Updated linter and useTheme

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,7 @@ module.exports = {
         },
     },
     rules: {
-        indent: ['error', 4],
+        indent: ['error', 4, { SwitchCase: 1 }],
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
