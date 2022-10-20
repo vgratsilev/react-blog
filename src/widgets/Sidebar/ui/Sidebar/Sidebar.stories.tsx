f054078c8971b8dbfddf34b0d3cef2fca410531d
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2022-10-20 19:48:09 +0500

    Added Sidebar opened stories.

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { userEvent, within } from '@storybook/testing-library';
 import { Sidebar } from './Sidebar';
 
 export default {
@@ -9,13 +10,31 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    args: {},
 } as ComponentMeta<typeof Sidebar>;
 
 const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />;
 
 export const Light = Template.bind({});
-Light.args = {};
+
+export const LightOpened = Template.bind({});
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+LightOpened.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const toggleButton = await canvas.getByTestId('sidebar-toggle');
+    await userEvent.click(toggleButton);
+};
 
 export const Dark = Template.bind({});
-Dark.args = {};
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const DarkOpened = Template.bind({});
+DarkOpened.decorators = [ThemeDecorator(Theme.DARK)];
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+DarkOpened.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const toggleButton = await canvas.getByTestId('sidebar-toggle');
+    await userEvent.click(toggleButton);
+};
