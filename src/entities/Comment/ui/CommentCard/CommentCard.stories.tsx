1aa684eccb98f403ce2980bbff93ffcc8d49c912
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2022-11-08 00:44:47 +0500

    Updated Storybook.

--- src/entities/Comment/ui/CommentCard/CommentCard.stories.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.stories.tsx
@@ -1,6 +1,8 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { IComment } from 'entities/Comment';
+import AvatarTest from 'shared/assets/tests/storybookAvatar.png';
 import { CommentCard } from './CommentCard';
 
 export default {
@@ -13,9 +15,22 @@ export default {
 
 const Template: ComponentStory<typeof CommentCard> = (args) => <CommentCard {...args} />;
 
+const comment: IComment = {
+    id: '1',
+    text: 'comment 1',
+    user: { id: '1', username: 'user 1', avatar: AvatarTest },
+};
+
 export const Light = Template.bind({});
-Light.args = {};
+Light.args = { comment };
 
 export const Dark = Template.bind({});
-Dark.args = {};
+Dark.args = { comment };
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const IsLoadingLight = Template.bind({});
+IsLoadingLight.args = { isLoading: true };
+
+export const IsLoadingDark = Template.bind({});
+IsLoadingDark.args = { isLoading: true };
+IsLoadingDark.decorators = [ThemeDecorator(Theme.DARK)];
--- src/entities/Comment/ui/CommentList/CommentList.stories.tsx
+++ src/entities/Comment/ui/CommentList/CommentList.stories.tsx
@@ -1,6 +1,8 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import AvatarTest from 'shared/assets/tests/storybookAvatar.png';
+import { IComment } from 'entities/Comment';
 import { CommentList } from './CommentList';
 
 export default {
@@ -13,9 +15,37 @@ export default {
 
 const Template: ComponentStory<typeof CommentList> = (args) => <CommentList {...args} />;
 
+const comments: IComment[] = [
+    {
+        id: '1',
+        text: 'comment 1',
+        user: { id: '1', username: 'user 1', avatar: AvatarTest },
+    },
+    { id: '2', text: 'comment 2', user: { id: '2', username: 'user 2' } },
+    {
+        id: '3',
+        text: 'comment 3',
+        user: { id: '1', username: 'user 1', avatar: AvatarTest },
+    },
+];
+
 export const Light = Template.bind({});
-Light.args = {};
+Light.args = { comments };
 
 export const Dark = Template.bind({});
-Dark.args = {};
+Dark.args = { comments };
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const IsLoadingLight = Template.bind({});
+IsLoadingLight.args = { comments, isLoading: true };
+
+export const IsLoadingDark = Template.bind({});
+IsLoadingDark.args = { comments, isLoading: true };
+IsLoadingDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const NoCommentsLight = Template.bind({});
+NoCommentsLight.args = {};
+
+export const NoCommentsDark = Template.bind({});
+NoCommentsDark.args = {};
+NoCommentsDark.decorators = [ThemeDecorator(Theme.DARK)];
