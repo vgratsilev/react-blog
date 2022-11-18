4d43fed59e9a9a0be5ee237877e327fd6d2419d5
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2022-11-18 15:45:47 +0500

    Added Tabs UI component.

---
+++ src/shared/ui/Tabs/Tabs.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { action } from '@storybook/addon-actions';
+import { Tabs } from './Tabs';
+
+export default {
+    title: 'shared/Tabs',
+    component: Tabs,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    args: {
+        tabs: [
+            { value: 'tab 1', content: 'tab 1' },
+            { value: 'tab 2', content: 'tab 2' },
+            { value: 'tab 3', content: 'tab 3' },
+        ],
+        value: 'tab 2',
+        onTabClick: action('onTabClick'),
+    },
+} as ComponentMeta<typeof Tabs>;
+
+const Template: ComponentStory<typeof Tabs> = (args) => <Tabs {...args} />;
+
+export const Light = Template.bind({});
+
+export const Dark = Template.bind({});
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
---
+++ src/shared/ui/Tabs/Tabs.tsx
@@ -0,0 +1,41 @@
+import { memo, ReactNode, useCallback } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Card, CardTheme } from 'shared/ui/Card/Card';
+import cls from './Tabs.module.scss';
+
+export interface ITabItem {
+    value: string;
+    content: ReactNode;
+}
+
+interface ITabsProps {
+    className?: string;
+    tabs: ITabItem[];
+    value: string;
+    onTabClick: (tab: ITabItem) => void;
+}
+
+export const Tabs = memo((props: ITabsProps) => {
+    const { className, tabs, onTabClick, value } = props;
+
+    const onClickHandler = useCallback(
+        (tab: ITabItem) => () => {
+            onTabClick(tab);
+        },
+        [onTabClick],
+    );
+
+    return (
+        <div className={classNames(cls.tabs, {}, [className])}>
+            {tabs.map((tab) => (
+                <Card
+                    key={tab.value}
+                    theme={tab.value === value ? CardTheme.NORMAL : CardTheme.OUTLINED}
+                    onClick={onClickHandler(tab)}
+                >
+                    {tab.content}
+                </Card>
+            ))}
+        </div>
+    );
+});
