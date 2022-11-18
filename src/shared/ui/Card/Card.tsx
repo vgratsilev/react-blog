0fee55093a1d7518a4ec225e5511729d9e5ba9a4
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2022-11-18 15:45:16 +0500

    Added Card themes.

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -2,17 +2,23 @@ import { HTMLAttributes, ReactNode } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Card.module.scss';
 
+export enum CardTheme {
+    NORMAL = 'normal',
+    OUTLINED = 'outlined',
+}
+
 interface ICardProps extends HTMLAttributes<HTMLDivElement> {
     className?: string;
     children: ReactNode;
+    theme?: CardTheme;
 }
 
 export const Card = (props: ICardProps) => {
-    const { className, children, ...otherProps } = props;
+    const { className, children, theme = CardTheme.NORMAL, ...otherProps } = props;
     return (
         <div
             {...otherProps}
-            className={classNames(cls.card, {}, [className])}
+            className={classNames(cls.card, {}, [className, cls[theme]])}
         >
             {children}
         </div>
