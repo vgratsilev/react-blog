dd4457983e0147dcbeaf1d9720f89cfa4c52e344
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2022-11-28 22:02:01 +0500

    Added Headless UI. Added ListBox UI component.

--- .eslintrc.js
+++ .eslintrc.js
@@ -85,6 +85,7 @@ module.exports = {
                     'gap',
                     'TitleTag',
                     'Tag',
+                    'as',
                 ],
             },
         ],
---
+++ src/shared/ui/ListBox/ListBox.tsx
@@ -0,0 +1,67 @@
+import { Fragment, memo, ReactNode } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Listbox as HListBox } from '@headlessui/react';
+import { Button } from '../Button/Button';
+import cls from './ListBox.module.scss';
+
+export interface IListBoxItem {
+    value: string;
+    content: ReactNode;
+    disabled?: boolean;
+}
+
+interface IListBoxProps {
+    className?: string;
+    label?: string;
+    items?: IListBoxItem[];
+    value?: string;
+    defaultValue?: string;
+    onChange: <T extends string>(value: T) => void;
+    readonly?: boolean;
+}
+
+export const ListBox = memo((props: IListBoxProps) => {
+    const { className, label, items, value, defaultValue, onChange, readonly } = props;
+    return (
+        <HListBox
+            as={'div'}
+            className={classNames(cls.listbox, { [cls.disabled]: readonly }, [className])}
+            value={value}
+            onChange={onChange}
+            disabled={readonly}
+        >
+            <HListBox.Label className={cls.label}>{label}</HListBox.Label>
+            <HListBox.Button className={cls.button}>
+                <Button className={classNames('', { [cls.innerButtonDisabled]: readonly })}>
+                    {value ?? defaultValue}
+                </Button>
+            </HListBox.Button>
+            <HListBox.Options className={cls.options}>
+                {items?.map((item: IListBoxItem) => (
+                    <HListBox.Option
+                        key={item.value}
+                        value={item.value}
+                        disabled={item.disabled}
+                        as={Fragment}
+                    >
+                        {({ active, selected }) => (
+                            <li
+                                className={classNames(
+                                    cls.item,
+                                    {
+                                        [cls.active]: active,
+                                        [cls.disabled]: item.disabled,
+                                    },
+                                    [],
+                                )}
+                            >
+                                {selected && '>'}
+                                {item.content}
+                            </li>
+                        )}
+                    </HListBox.Option>
+                ))}
+            </HListBox.Options>
+        </HListBox>
+    );
+});
