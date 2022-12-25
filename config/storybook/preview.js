0f26c2c4c0f7c4cc93ab51323ff25cf12a96dc11
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2022-12-26 00:08:14 +0500

    Added theme addon for storybook.

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -15,6 +15,14 @@ export const parameters = {
         },
     },
     layout: 'fullscreen',
+    themes: {
+        default: 'light',
+        list: [
+            { name: 'light', class: Theme.LIGHT, color: '#ffffff' },
+            { name: 'dark', class: Theme.DARK, color: '#000000' },
+            { name: 'orange', class: Theme.ORANGE, color: '#ffb005' },
+        ],
+    },
 };
 
 export const globalTypes = {
