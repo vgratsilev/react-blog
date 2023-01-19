d1440fafe2c4e21bfbb35ac48b1acd4349436f0a
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2023-01-19 17:16:01 +0500

    Updated icon loader.

--- config/build/loaders/buildSvgLoader.ts
+++ config/build/loaders/buildSvgLoader.ts
@@ -3,6 +3,36 @@ export function buildSvgLoader() {
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
         resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
-        use: ['@svgr/webpack'],
+        use: [
+            {
+                loader: '@svgr/webpack',
+                options: {
+                    svgoConfig: {
+                        plugins: [
+                            {
+                                name: 'preset-default',
+                                params: {
+                                    overrides: {
+                                        removeViewBox: false,
+                                    },
+                                },
+                            },
+                            // {
+                            //     name: 'convertColors',
+                            //     params: {
+                            //         currentColor: true,
+                            //     },
+                            // },
+                            // {
+                            //     name: 'addClassesToSVGElement',
+                            //     params: {
+                            //         className: 'app-icon',
+                            //     },
+                            // },
+                        ],
+                    },
+                },
+            },
+        ],
     };
 }
